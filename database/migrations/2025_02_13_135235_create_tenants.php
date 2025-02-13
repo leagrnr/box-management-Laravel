4c39f2cf90635752c2ff69ed4b543a9464ec936a
ElRacoon <lea.granier@outlook.fr>
2025-02-13 16:47:54 +0100

    migration Model

---
+++ database/migrations/2025_02_13_135235_create_tenants.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tenants', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('email');
+            $table->string('telephone');
+            $table->string('profession');
+            $table->string('adresse');
+            $table->string('ville');
+            $table->string('pays');
+            $table->string('date_de_naissance');
+            $table->string('lieu_de_naissance');
+            $table->string('compte_bancaire');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tenants');
+    }
+};
