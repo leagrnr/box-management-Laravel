375bba29eb5174130866cd547fe335923107cf98
ElRacoon <lea.granier@outlook.fr>
2025-02-13 16:47:35 +0100

    CRUD Model

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\BoxController;
+use App\Http\Controllers\TenantController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,6 +14,8 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
+//    BOXES
     Route::get('/boxes', [BoxController::class, 'index'])->name('boxes.index');
     Route::get('/boxes/create', [BoxController::class, 'create'])->name('boxes.create');
     Route::get('/boxes/{id}', [BoxController::class, 'show'])->name('boxes.show');
@@ -21,7 +24,16 @@ Route::middleware('auth')->group(function () {
     Route::put('/boxes/{id}/update', [BoxController::class, 'update'])->name('boxes.update');
     Route::delete('/boxes/{id}', [BoxController::class, 'destroy'])->name('boxes.destroy');
 
+//    TENANTS
+    Route::get('/tenants', [TenantController::class, 'index'])->name('tenants.index');
+    Route::get('/tenants/create', [TenantController::class, 'create'])->name('tenants.create');
+    Route::get('/tenants/{id}', [TenantController::class, 'show'])->name('tenants.show');
+    Route::post('/tenants', [TenantController::class, 'store'])->name('tenants.store');
+    Route::get('/tenants/{id}/edit', [TenantController::class, 'edit'])->name('tenants.edit');
+    Route::put('/tenants/{id}/update', [TenantController::class, 'update'])->name('tenants.update');
+    Route::delete('/tenants/{id}', [TenantController::class, 'destroy'])->name('tenants.destroy');
 
+//    PROFILE
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
