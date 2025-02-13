8e96cf0459aa072387e7b463ca523a513a5f253d
ElRacoon <lea.granier@outlook.fr>
2025-02-13 13:44:01 +0100

    link user-box in User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function boxes()
+    {
+        return $this->hasMany(Box::class);
+    }
 }
