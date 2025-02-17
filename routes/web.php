91aa83818bd61fbafd95078f726e091850b75e06
ElRacoon <lea.granier@outlook.fr>
2025-02-17 09:52:41 +0100

    route contract model

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\BoxController;
 use App\Http\Controllers\TenantController;
+use App\Http\Controllers\ContractModelController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -32,6 +33,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/tenants/{id}/edit', [TenantController::class, 'edit'])->name('tenants.edit');
     Route::put('/tenants/{id}/update', [TenantController::class, 'update'])->name('tenants.update');
     Route::delete('/tenants/{id}', [TenantController::class, 'destroy'])->name('tenants.destroy');
+
+// CONTRACTS MODEL
+    Route::get('/contracts_model', [ContractModelController::class, 'index'])->name('contracts_model.index');
+    Route::get('/contracts_model/create', [ContractModelController::class, 'create'])->name('contracts_model.create');
+    Route::get('/contracts_model/{id}', [ContractModelController::class, 'show'])->name('contracts_model.show');
+    Route::post('/contracts_model', [ContractModelController::class, 'store'])->name('contracts_model.store');
+    Route::get('/contracts_model/{id}/edit', [ContractModelController::class, 'edit'])->name('contracts_model.edit');
+    Route::put('/contracts_model/{id}/update', [ContractModelController::class, 'update'])->name('contracts_model.update');
+    Route::delete('/contracts_model/{id}', [ContractModelController::class, 'destroy'])->name('contracts_model.destroy');
 
 //    PROFILE
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
