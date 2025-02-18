d5f78be9f0bbfe9e549422d7f5ad62c5c8ba7e08
ElRacoon <lea.granier@outlook.fr>
2025-02-18 09:49:53 +0100

    create migration for bills

---
+++ database/migrations/2025_02_17_221306_create_bills.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('contract_id')->constrained()->cascadeOnDelete();
+            $table->date('payment_date')->nullable();
+            $table->date('payment_montant');
+            $table->date('period_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bills');
+    }
+};
